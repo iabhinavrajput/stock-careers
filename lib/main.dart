39eff0cb983183e14dd773b59e7d78d91dd97d18
iabhinavrajput <abhinavrajput207@gmail.com>
2025-04-15 16:26:31 +0530

    google fonts added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'blocs/auth/auth_bloc.dart';
 import 'data/services/auth_service.dart';
 import 'routes/app_routes.dart';
@@ -37,6 +38,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme, 
+        ),
       ),
       initialRoute: isLoggedIn ? AppRoutes.home : AppRoutes.login,
       onGenerateRoute: AppRoutes.generateRoute,
